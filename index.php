c4368f67ef8da15b875691360008fa8aa315ead4
Christian Aldridge <Studiozut@gmail.com>
2013-06-19 16:36:17 -0300

    Changed jquery to 1.10.1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 <title>Obento</title>
 <!-- jquery -->
-  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 </head>
 <body>
 <article>
