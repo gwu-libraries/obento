34dacee93be72670b6d52d2108e38d1c8872bdf9
Christian Aldridge <Studiozut@gmail.com>
2013-06-19 16:13:50 -0300

    Base index file

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Obento</title>
+<!-- jquery -->
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+</head>
+<body>
+<article>
+<h1>GW Libraries Obento</h1>
+</article>
+</body>
+</html>
