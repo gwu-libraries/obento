fe7509ca3f942cdd306461f5d769945c0c0f1977
Christian Aldridge <studiozut@gmail.com>
2013-06-21 09:41:03 -0400

    added comments to index.php; tweaked json text in summon.php in order to test

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <script type="text/javascript" src="number_format.js"></script>
 </head>
 
+<!-- this script will be auto gen'd -->
 <script type="text/javascript">
     var url_encoded_query = "moby+dick";
     var display_query = "moby dick";
@@ -22,7 +23,9 @@
 
 <div id="articles">
 	<h2 class="red" id="articlesHeader">Articles</h2>
+	<!-- this is where the summon.js dumps the results, parsed from the summon.php json -->
 	<div id="articleSearchResultsItems">
+	<!-- spinner works! visibility (hide when loaded) controlled by summon.js -->
 	<img id="articleSearchResultsSpinner" src="spinner.gif" alt="Loading..." width="16" height="16" /></div>
 </div>
 
