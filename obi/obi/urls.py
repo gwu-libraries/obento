ccc1b9dd0e9007d8242bc3aa7ac75417429b1770
Daniel Chudnov <dchud@gwu.edu>
2013-07-17 12:17:08 -0400

    adding json/html views for databases, refs #5

--- obi/obi/urls.py
+++ obi/obi/urls.py
@@ -8,7 +8,9 @@ urlpatterns = patterns('',
     url(r'^$', 'ui.views.home', name='home'),
 
     url(r'^aquabrowser', 'ui.views.aquabrowser', name='aquabrowser'),
-    url(r'^databases', 'ui.views.databases', name='databases'),
+
+    url(r'^databases_html', 'ui.views.databases_html', name='databases_html'),
+    url(r'^databases_json', 'ui.views.databases_json', name='databases_json'),
 
     url(r'^admin/', include(admin.site.urls)),
 
--- obi/ui/views.py
+++ obi/ui/views.py
@@ -10,6 +10,10 @@ from django.shortcuts import render
 from ui.models import Database
 
 
+# FIXME: make a local_setting
+DEFAULT_HIT_COUNT = 3
+
+
 def home(request):
     q = request.GET.get('q', None)
     return render(request, 'home.html', {
@@ -27,8 +31,12 @@ def aquabrowser(request):
     return HttpResponse(r.content, content_type='application/xml')
 
 
-def databases(request):
+def _databases_query(request):
     q = request.GET.get('q', None)
+    try:
+        count = request.GET.get('count', DEFAULT_HIT_COUNT)
+    except:
+        count = DEFAULT_HIT_COUNT
     response = {'q': q}
     if q:
         matches = []
@@ -37,9 +45,21 @@ def databases(request):
                                                Q(description__icontains=q))
         response['count_total'] = qs_databases.count()
         response['more_url'] = '%s%s' % (settings.DATABASES_MORE_URL, q)
-        for db in qs_databases[:10]:
+        for db in qs_databases[:count]:
             match = {'name': db.name, 'url': db.url,
                      'description': db.description}
             matches.append(match)
         response['matches'] = matches
+    return response
+
+
+def databases_html(request):
+    response = _databases_query(request)
+    return render(request, 'databases.html', {
+        'response': response,
+    })
+
+
+def databases_json(request):
+    response = _databases_query(request)
     return HttpResponse(json.dumps(response), content_type='application/json')
