c9e5ba7fac1f888661bd71dca3e8b540f56e2ad2
Daniel Chudnov <dchud@gwu.edu>
2013-07-25 11:53:48 -0400

    adding admin view, adding xlrd to requirements, whitespace in models, refs #36

--- obi/ui/admin.py
+++ obi/ui/admin.py
@@ -7,3 +7,9 @@ class DatabaseAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'url']
     search_fields = ['name', 'description', 'url']
 admin.site.register(m.Database, DatabaseAdmin)
+
+
+class JournalAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'issn', 'eissn']
+    search_fields = ['title', 'issn', 'eissn']
+admin.site.register(m.Journal, JournalAdmin)
