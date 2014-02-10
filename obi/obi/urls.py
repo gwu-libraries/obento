be3a8a90be684396bc9713464a5ee68c2079b22e
kerchner <kerchner@gwu.edu>
2014-02-10 15:06:47 -0500

    removed service status html URL

--- obi/obi/urls.py
+++ obi/obi/urls.py
@@ -62,8 +62,6 @@ urlpatterns = patterns('ui.views',
     url(r'^libsite_html', 'libsite_html', name='libsite_html'),
     url(r'^libsite_json', 'libsite_json', name='libsite_json'),
 
-    url(r'^healthcheck_html', 'healthcheck_html',
-        name='healthcheck_html'),
     url(r'^summon_healthcheck_json', 'summon_healthcheck_json',
         name='summon_healthcheck_json'),
 )
