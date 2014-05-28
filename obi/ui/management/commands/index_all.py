43820cda9d17444193bc6d81439940cdfa5ca4ad
kerchner <kerchner@gwu.edu>
2014-05-28 10:51:34 -0400

    Fixes #236

--- obi/ui/management/commands/index_all.py
+++ obi/ui/management/commands/index_all.py
@@ -1,7 +1,9 @@
+from optparse import make_option
+import requests
+import solr
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
-
-import solr
 
 from ui.models import Database, Journal
 
@@ -11,10 +13,19 @@ COMMIT_BLOCK_SIZE = 100
 
 class Command(BaseCommand):
     help = 'index the database list and journal titles list'
+    option_list = BaseCommand.option_list + (
+        make_option('--no-clear', action="store_true", default=False,
+                    help='Do not clear out the solr index.'),
+        )
 
     def handle(self, *args, **options):
-        # TODO: add an option for zapping the index first
         s = solr.SolrConnection(settings.SOLR_URL)
+        # unless the user gives a --no-clear option, clear the solr index.
+        if not options.get('no-clear', None):
+            requests.get(settings.SOLR_URL + '/update' +
+                         '?stream.body=<delete><query>*:*</query></delete>')
+            requests.get(settings.SOLR_URL + '/update' +
+                         '?stream.body=<commit/>')
         qs_databases = Database.objects.all()
         total_indexed = 0
         block = []
