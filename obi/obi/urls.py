9f7af379f2de74218a268425ea5dea6254df5f74
kerchner <kerchner@gwu.edu>
2015-02-13 13:43:32 -0500

    Added bentobubbles template

--- obi/obi/urls.py
+++ obi/obi/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -67,6 +68,10 @@ urlpatterns = patterns('ui.views',
 
     url(r'^searches', 'searches', name='searches'),
     url(r'^topsearches_json', 'topsearches_json', name='topsearches_json'),
+
+    url(r'^bentobubbles',
+        TemplateView.as_view(template_name='bentobubbles.html'),
+        name='bentobubbles'),
 )
 
 urlpatterns += patterns('',
