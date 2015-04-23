ed99c2387b8c2bf970a68546352ce01b053effca
MustafaDasorwala <mustafa.dasorwala@yahoo.com>
2015-04-23 12:55:39 -0400

    updated migration scripts. fixes #324

--- obi/ui/migrations/0001_initial.py
+++ obi/ui/migrations/0001_initial.py
@@ -35,6 +35,11 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('q', models.TextField(db_index=True, blank=True)),
                 ('date_searched', models.DateTimeField(auto_now_add=True, db_index=True)),
+                ('articles_count', models.BigIntegerField(default=0, blank=True)),
+                ('books_count', models.BigIntegerField(default=0, blank=True)),
+                ('database_count', models.BigIntegerField(default=0, blank=True)),
+                ('journals_count', models.BigIntegerField(default=0, blank=True)),
+                ('researchguides_count', models.BigIntegerField(default=0, blank=True)),
             ],
             options={
                 'verbose_name_plural': 'searches',
