e7ca248bd3283596a9096b45586ece94a1ebbbaf
kerchner <kerchner@gwu.edu>
2015-11-06 10:45:31 -0500

    updated command help

--- obi/ui/management/commands/index_databases.py
+++ obi/ui/management/commands/index_databases.py
@@ -6,7 +6,7 @@ from ui.utils import index_dbs
 
 
 class Command(BaseCommand):
-    help = 'index the database list and journal titles list'
+    help = 'index the database list'
     option_list = BaseCommand.option_list + (
         make_option('--no-clear', action="store_true", default=False,
                     help='Do not clear out the solr index.'),
