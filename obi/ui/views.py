626a8e5454384dbb39172498553d21d0bacfe937
kerchner <kerchner@gwu.edu>
2017-01-03 08:53:50 -0500

    Removed debug logging, cleaned up most PEP8. Fixes #389

--- obi/ui/views.py
+++ obi/ui/views.py
@@ -306,7 +306,6 @@ def _summon_id_string(accept, xsummondate, host, path, params):
 
 
 def _summon_query(request, scope='all'):
-    logger = logging.getLogger('django.request')
     headers = {'Accept': 'application/json'}
     headers['Host'] = settings.SUMMON_HOST
     headers['x-summon-date'] = datetime.datetime.utcnow().strftime(
@@ -423,9 +422,10 @@ def _summon_query(request, scope='all'):
                 # If summon isn't already resolving to the resource
                 # (in which case, great, use resolved_url!),
                 # then see if we can do better
-                if (resolved_url.startswith('http://findit.library.gwu') or
-                   resolved_url.startswith('https://findit.library.gwu')):
-                    # Construct the OpenURL request and get the OpenURL XML document
+                if resolved_url.startswith('http://findit.library.gwu') or \
+                   resolved_url.startswith('https://findit.library.gwu'):
+                    # Construct the OpenURL request
+                    # and get the OpenURL XML document
                     xmlurl = 'http://uz4ug4lz9g.openurl.xml.serialssolutions.com/openurlxml?version=1.0&' + document['openUrl']
                     f = urllib2.urlopen(xmlurl)
                     tree = f.read()
@@ -436,8 +436,9 @@ def _summon_query(request, scope='all'):
                     # {child-node-type: [child-node, child-node, ...]}
                     # We wish to force it to always create a list
                     data = xmltodict.parse(tree,
-                            force_list=('ssopenurl:result', 'ssopenurl:linkGroup',
-                                        'ssopenurl:url',))
+                                           force_list=('ssopenurl:result',
+                                                       'ssopenurl:linkGroup',
+                                                       'ssopenurl:url',))
                     match['OpenUrl'] = xmlurl
 
                     # Traverse the tree and look for the node(s) containing
@@ -446,32 +447,25 @@ def _summon_query(request, scope='all'):
                     try:
                         for result in results:
                             if not 'ssopenurl:linkGroups' in result:
-                                logger.error("  TRIED BUT DIDN'T FIND ONE, USING FINDIT")
                                 break
                             linkgroups = result['ssopenurl:linkGroups']['ssopenurl:linkGroup']
-                            foundone = False
+                            found_article_url = False
                             for lg in linkgroups:
                                 urlnodes = lg['ssopenurl:url']
                                 articleurls = [node['#text'] for node in urlnodes if '@type' in node and node['@type'] == 'article']
                                 if articleurls:
-                                    # If there is one, replace with the [first] OpenURL result.
+                                    # If there is one, replace with
+                                    # the [first] OpenURL result.
                                     match['url'] = articleurls[0]
                                     # Done, stop here
-                                    logger.error("  FOUND ONE")
-                                    foundone = True
+                                    found_article_url = True
                                     break
-                            if foundone:
+                            if found_article_url:
+                                # stop looping through results
                                 break
-                        else:
-                            logger.error("  TRIED BUT DIDN'T FIND ONE, USING FINDIT")
-                    except (TypeError, IndexError, KeyError) as e:
+                    except (TypeError, IndexError, KeyError):
                         # If there's an error, just stick with the resolved url
-                        logger.error("  TRIED BUT GOT ERROR, USING FINDIT")
-                        logger.error("  xmlurl = " + xmlurl)
-                        raise e
                         pass
-                else:
-                    logger.error("  ALREADY HAVE RESOLVED LINK")
 
         matches.append(match)
 
