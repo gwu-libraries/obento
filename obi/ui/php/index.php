2115fc1c196822d2a4f9aff041821bbb73112aa0
Dan Kerchner <kerchner@users.noreply.github.com>
2017-11-09 16:50:07 -0500

    Updated fallback bentoTarget server URL

--- obi/ui/php/index.php
+++ obi/ui/php/index.php
@@ -54,7 +54,7 @@
             count="<?php if (isset($_GET["count"])) {print $_GET["count"];} else {print "0";}?>";
             <?php
   	      // checks to see if function exists and sets the Bento target (set in GW custom Catalog Pointer module) and if not sets a default value (the prod Bento server).
-  	      if (function_exists('catalog_pointer_bento')) {$bentoTarget = catalog_pointer_bento();} else {$bentoTarget = "http://gwbento-prod.wrlc.org:8080/";} 
+  	      if (function_exists('catalog_pointer_bento')) {$bentoTarget = catalog_pointer_bento();} else {$bentoTarget = "http://bento.library.gwu.edu/";} 
 	    ?>
             bento_url = "<?php echo $bentoTarget; ?>";
 	    var save_data_url = "save_data";
